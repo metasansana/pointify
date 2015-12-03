7e23f3cba3b46158c478e48b76a0a2c4b8e2535b
Lasana Murray <lmurray@quenk.com>
2015-12-03 10:45:00 -0400

    Adding more tests

--- test/test.js
+++ test/test.js
@@ -15,82 +15,97 @@ function bundle(file) {
 
 describe('bundle transform', function() {
 
-    beforeEach(function() {
-        result = {};
-    });
+            beforeEach(function() {
+                result = {};
+            });
 
-    it('should work', function(done) {
+            it('should work', function(done) {
 
-        bundle(__dirname + '/../examples/person.json').bundle(function(err, src) {
+                bundle(__dirname + '/../examples/person.json').bundle(function(err, src) {
 
-            if (err) return done(err);
+                    if (err) return done(err);
 
-            must(result.data).to.be('{"name":"Lasana Murray",' +
-                '"address":{"street_address":"1600 Pennsylvania Avenue NW",' +
-                '"city":"Washington","state":"DC","type":"business","phone":"253-4444"}}');
-            done();
-        });
+                    must(result.data).to.be('{"name":"Lasana Murray",' +
+                        '"address":{"street_address":"1600 Pennsylvania Avenue NW",' +
+                        '"city":"Washington","state":"DC","type":"business","phone":"253-4444"}}');
+                    done();
+                });
 
-    });
+            });
 
-    it('should work with arrays', function(done) {
+            it('should work with arrays', function(done) {
 
-        bundle(__dirname + '/../examples/addressBook.json').bundle(function(err, src) {
+                bundle(__dirname + '/../examples/addressBook.json').bundle(function(err, src) {
 
-            if (err) return done(err);
+                    if (err) return done(err);
 
-            must(result.data).to.be(
-                '{"owner":"Shaina Purple","addresses":' +
-                '[{"street_address":"1600 Pennsylvania Avenue NW",' +
-                '"city":"Washington","state":"DC","type":"business"},' +
-                '{"street_address":"1600 Pennsylvania Avenue NW","city":"Washington",' +
-                '"state":"DC","type":"business"},{"street_address":' +
-                '"1600 Pennsylvania Avenue NW","city":"Washington","state":"DC",' +
-                '"type":"business"}]}');
-            done();
-        });
-    });
+                    must(result.data).to.be(
+                        '{"owner":"Shaina Purple","addresses":' +
+                        '[{"street_address":"1600 Pennsylvania Avenue NW",' +
+                        '"city":"Washington","state":"DC","type":"business"},' +
+                        '{"street_address":"1600 Pennsylvania Avenue NW","city":"Washington",' +
+                        '"state":"DC","type":"business"},{"street_address":' +
+                        '"1600 Pennsylvania Avenue NW","city":"Washington","state":"DC",' +
+                        '"type":"business"}]}');
+                    done();
+                });
+            });
 
-    it('should work recursively', function(done) {
+            it('should work recursively', function(done) {
 
-        bundle(__dirname + '/../examples/splitPerson.json').bundle(function(err, src) {
+                bundle(__dirname + '/../examples/splitPerson.json').bundle(function(err, src) {
 
-            if (err) return done(err);
+                    if (err) return done(err);
 
-            must(result.data).to.be('{"name":"Lasana Murray",' +
-                '"alterEgo":{"name":"Lasana Murray","address":' +
-                '{"street_address":"1600 Pennsylvania Avenue NW","city":"Washington",' +
-                '"state":"DC","type":"business","phone":"253-4444"}}}');
-            done();
-        });
+                    must(result.data).to.be('{"name":"Lasana Murray",' +
+                        '"alterEgo":{"name":"Lasana Murray","address":' +
+                        '{"street_address":"1600 Pennsylvania Avenue NW","city":"Washington",' +
+                        '"state":"DC","type":"business","phone":"253-4444"}}}');
+                    done();
+                });
 
-    });
+            });
 
-    it('should work with paths', function(done) {
+            it('should work with paths', function(done) {
 
-        bundle(__dirname + '/json/paths.json').bundle(function(err, src) {
+                bundle(__dirname + '/json/paths.json').bundle(function(err, src) {
 
-            if (err) return done(err);
+                    if (err) return done(err);
 
-            must(result.data).to.be('{"values":{"name":"Lasana Murray",' +
-                '"paths":["x","y","z"]}}');
-            done();
-        });
+                    must(result.data).to.be('{"values":{"name":"Lasana Murray",' +
+                        '"paths":["x","y","z"]}}');
+                    done();
+                });
 
-    });
+            });
 
-    it('should work with embedded strings', function(done) {
+            it('should work with embedded strings', function(done) {
 
-        bundle(__dirname + '/json/emb.json').bundle(function(err, src) {
+                bundle(__dirname + '/json/emb.json').bundle(function(err, src) {
 
-            if (err) return done(err);
+                    if (err) return done(err);
 
-            must(result.data).to.be('{"values":{"name":"Lasana Murray",' +
-                '"paths":["x","y","z"]}}');
-            done();
-        });
+                    must(result.data).to.be('{"values":{"name":"Lasana Murray",' +
+                        '"paths":["x","y","z"]}}');
+                    done();
+                });
 
-    });
+            });
+
+            it('should not resolve the parent before child if they both have a matching file name', function(done) {
+
+                    bundle(__dirname + '/parent/child/n.json').
+                    bundle(function(err, src) {
+
+                            if (err) return done(err);
+
+                            must(result.data).to.be('{"a":{"name":"child foot"}}');
+                                done();
+
+                            });
 
 
-});
+                    });
+
+
+            });
